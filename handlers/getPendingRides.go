68a0776ea3430dc0adcdad0ae1f90331b33d9abc
Nipun2001M <nipun20010328@gmail.com>
2025-02-13 22:36:43 +0530

    unwanted prints removed

--- handlers/getPendingRides.go
+++ handlers/getPendingRides.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"ride-sharing-service/connection"
 	"ride-sharing-service/model"
@@ -11,23 +10,18 @@ import (
 
 func GetPendingRides(w http.ResponseWriter, req *http.Request) {
     riderid := req.URL.Query().Get("riderId")
-    fmt.Println(riderid)
 
     var rider model.Rider
     if err := connection.Db.First(&rider, riderid).Error; err != nil {
         http.Error(w, "cannot find rider details", http.StatusBadRequest)
         return
     }
-    
-    fmt.Println("rider----", rider.VehicleType)
-
     var matchingRides []model.Ride
     if err := connection.Db.Where("status = ? AND vehical_type = ?", "PENDING", rider.VehicleType).Find(&matchingRides).Error; err != nil {
         http.Error(w, "error in getting matching rides", http.StatusBadRequest)
         return
     }
 
-    fmt.Println("------", matchingRides)
     w.Header().Set("Content-Type", "application/json")
 	var matchingridesRes []response.MatchingPendingRidesResponse
 	for _,i:=range matchingRides{
